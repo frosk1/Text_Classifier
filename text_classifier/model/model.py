7f589d84f4023d35784273780b6e10fa9b373fad
Jan <jan-ek@web.de>
2015-08-29 18:06:10 +0200

    added evaluate_classification_report.

--- text_classifier/model/model.py
+++ text_classifier/model/model.py
@@ -6,6 +6,7 @@ from sklearn import tree
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 from sklearn.cross_validation import KFold
 from sklearn.metrics import accuracy_score
+from sklearn import metrics
 
 '''
 Class Model :
@@ -43,7 +44,7 @@ class Model(object):
         :return:
         """
         if classifier_name == "svm":
-            self.clf = svm.SVC()
+            self.clf = svm.SVC(kernel="linear")
         elif classifier_name == "naive_bayes":
             self.clf = GaussianNB()
         elif classifier_name == "decision_tree":
@@ -74,33 +75,54 @@ class Model(object):
         else:
             print self.clf.predict(sample)
 
-    def evaluate(self, folds):
+    def evaluate_cross_validation(self, folds):
         """
 
         :param folds:
         :return:
         """
-        kf = KFold(len(self.feature_samples), n_folds=folds)
-        accuracy_list = []
+        if self.clf is None and self.feature_samples is None:
+            print "please set an classifier first and fill feature, target"
 
-        for train, test in kf:
+        elif folds > len(self.feature_samples):
+            print "There are " + str(len(self.feature_samples)) + " samples in the feature_samples."+"\n" + \
+                  "The folds has to be even or less than "+str(len(self.feature_samples)) + "."
 
-            x_train, x_test, y_train, y_test = self.feature_samples[train], self.feature_samples[test], \
-                                               self.targets[train], self.targets[test]
+        else:
+            kf = KFold(len(self.feature_samples), n_folds=folds)
+            accuracy_list = []
 
-            self.clf.fit(x_train, y_train)
-            accuracy_list.append(accuracy_score(np.array(y_test), np.array(self.clf.predict(x_test))))
+            for train, test in kf:
+                #print "train",train," test ",test
+                #print "len train", len(train), "test",len(test)
+                x_train, x_test, y_train, y_test = self.feature_samples[train], self.feature_samples[test], \
+                                                   self.targets[train], self.targets[test]
 
-        n = 0
-        sum_values = 0
+                self.clf.fit(x_train, y_train)
+                accuracy_list.append(accuracy_score(np.array(y_test), np.array(self.clf.predict(x_test))))
 
-        for acc_value in accuracy_list:
+            n = 0
+            sum_values = 0
 
-            sum_values = sum_values + acc_value
-            n += 1
+            for acc_value in accuracy_list:
 
-        acc_mean = (sum_values/n)
+                sum_values = sum_values + acc_value
+                n += 1
 
-        return accuracy_list, acc_mean
+            acc_mean = (sum_values/n)
 
+            return accuracy_list, acc_mean
 
+    def evaluate_classification_report(self, fraction):
+        if self.clf is None and self.feature_samples is None:
+            print "please set an classifier first and fill feature, target"
+
+        else:
+            count = int(round((float(len(self.targets))/float(100))*float(fraction), 0))
+            count2 = len(self.targets)-count
+
+            # sollte auf 100 % /fraction trainiert werden, wird auch auf 100% getestet
+            # wenn count2 0 ist (bei 100% count), dann wird self.targets[-count2:] zu self.targets[:]
+            self.clf.fit(self.feature_samples[:count], self.targets[:count])
+            predicted = self.clf.predict(self.feature_samples[-count2:])
+            print(metrics.classification_report(self.targets[-count2:], predicted, target_names=["0", "1"]))
