aa8942ab5b2015e1caacc627914794833d951a23
Jan <jan-ek@web.de>
2015-09-21 11:46:48 +0200

    Add passive_attribute.

--- text_classifier/head/feature.py
+++ text_classifier/head/feature.py
@@ -4,6 +4,8 @@ from text_classifier.attributes.tf_idf import TfIdf
 from text_classifier.attributes.readability import Readability
 from text_classifier.attributes.variety_count import Variety
 from text_classifier.attributes.perfect_tense import PerfectTense
+from text_classifier.attributes.nested_sentence import NestedSentence
+from text_classifier.attributes.passive import Passive
 from text_classifier.exceptions import FeatureNotExistException
 
 __author__ = 'jan'
@@ -32,8 +34,15 @@ class Feature(object):
             return attribute
         elif attribute_name == "variety":
             attribute = Variety()
+            return attribute
         elif attribute_name == "perfect_tense":
             attribute = PerfectTense()
+            return attribute
+        elif attribute_name == "nested_sentence":
+            attribute = NestedSentence()
+            return attribute
+        elif attribute_name == "passive":
+            attribute = Passive()
             return attribute
         else:
             raise FeatureNotExistException(attribute_name)
