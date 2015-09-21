28765376112518311849822baaacc5b3dff48a3d
Jan <jan-ek@web.de>
2015-09-21 11:45:42 +0200

    Implemented perfect_tense attribute.

--- text_classifier/attributes/perfect_tense.py
+++ text_classifier/attributes/perfect_tense.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from text_classifier.attributes.attribute import Attribute
-import re
-import collections
 import treetaggerwrapper
-from nltk.corpus import stopwords
+
 __author__ = 'jan'
 
 
@@ -12,7 +10,6 @@ class PerfectTense(Attribute):
     def __init__(self):
         self._name = "perfect_tense"
         self._text_set = None
-        # self.stopwords = stopwords.words("german")
         self.perf_count = dict()
         self.tagger = treetaggerwrapper.TreeTagger(TAGLANG='de')
         self.perf_tag_list = [u"VVPP", u"VMPP", u"VAPP"]
@@ -35,10 +32,12 @@ class PerfectTense(Attribute):
 
     def compute(self):
         for text in self._text_set:
+
             self.perf_count[text.id] = 0
             tags = treetaggerwrapper.make_tags(self.tagger.tag_text(text.text.decode("utf-8")))
 
             for tuple_tag in tags:
+                print tuple_tag
                 if tuple_tag[1] in self.perf_tag_list:
                     self.perf_count[text.id] += 1
 
