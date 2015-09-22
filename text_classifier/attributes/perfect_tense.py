cfd5aaf9f5445536e4dee70b23809d5840a6630e
Jan <jan-ek@web.de>
2015-09-22 11:25:30 +0200

    Implemented parsing for perfect_tense.

--- text_classifier/attributes/perfect_tense.py
+++ text_classifier/attributes/perfect_tense.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from text_classifier.attributes.attribute import Attribute
-import treetaggerwrapper
+from text_classifier.parzu import *
 
 __author__ = 'jan'
 
@@ -10,9 +10,9 @@ class PerfectTense(Attribute):
     def __init__(self):
         self._name = "perfect_tense"
         self._text_set = None
-        self.perf_count = dict()
-        self.tagger = treetaggerwrapper.TreeTagger(TAGLANG='de')
-        self.perf_tag_list = [u"VVPP", u"VMPP", u"VAPP"]
+        self.analyses_list = None
+        self.id_len_tuples = None
+        self.analyses_dict = None
 
     @property
     def name(self):
@@ -31,17 +31,22 @@ class PerfectTense(Attribute):
         self._text_set = new_value
 
     def compute(self):
+        self.id_len_tuples, self.analyses_list = parse_analyses(self._text_set)
+        self.analyses_dict = fill_analyses_dict(self.id_len_tuples, self.analyses_list)
+
         for text in self._text_set:
+            text.features["perfect_tense"] = self.count_perfect(text.id)
 
-            self.perf_count[text.id] = 0
-            tags = treetaggerwrapper.make_tags(self.tagger.tag_text(text.text.decode("utf-8")))
+    def count_perfect(self, text_id):
+        perf_count = 0
+        for analyses in self.analyses_dict[text_id]:
+            pattern = re.search("<-',\[mainclause,(\w+)_VAFIN,(\w+)_VVPP]", analyses)
+            if pattern:
+                s1 = pattern.group(1)
+                s2 = pattern.group(2)
+                pattern2 = re.search(".*" + s1 + "#(\d+).*" + s2 + "#(\d+).*", analyses)
+                if pattern2:
+                    if int(pattern2.group(2))-int(pattern2.group(1)) > 3:
+                        perf_count += 1
 
-            for tuple_tag in tags:
-                print tuple_tag
-                if tuple_tag[1] in self.perf_tag_list:
-                    self.perf_count[text.id] += 1
-
-            text.features["perfect_tense"] = self.perf_count[text.id]
-
-
-
+        return perf_count
