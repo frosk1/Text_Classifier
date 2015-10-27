b29ff7f95df8595c91db458b3e1f3560a75478ae
Jan <jan-ek@web.de>
2015-10-27 16:46:51 +0100

    implemented own BOW Model

--- text_classifier/attributes/bag_of_words.py
+++ text_classifier/attributes/bag_of_words.py
@@ -37,53 +37,66 @@ class BagOfWords(Attribute):
 
     def compute(self):
         self.build_model()
-        c = 0
+        # c = 0
 
         if self.bow_model is not None:
             print "BOW not None"
-            vectorizer = self.bow_model
-            X = vectorizer.transform(self.sentence_list)
-            # print vectorizer.get_feature_names()
-            A = X.toarray()
-            for text in self._text_set:
-                end_list = []
-                for i in A[c]:
-                    end_list.append(i)
+            # vectorizer = self.bow_model
+            # X = vectorizer.transform(self.sentence_list)
+            # # print vectorizer.get_feature_names()
+            # A = X.toarray()
 
-                text.features["bag_of_words"] = end_list
-                c += 1
+            for text in self._text_set:
+                temp_model = dict(self.bow_model)
+                for word in text.wordlist_lower:
+                    try:
+                        temp_model[word] += 1
+                    except KeyError:
+                        continue
+                text.features["bag_of_words"] = temp_model.values()
+
+                # end_list = []
+                # for i in A[c]:
+                #     end_list.append(i)
+                #
+                # text.features["bag_of_words"] = end_list
+                # c += 1
         else:
             print "BOW is None"
-            vectorizer = CountVectorizer(min_df=1)
-            X = vectorizer.fit_transform(self.sentence_list)
-            # print vectorizer.get_feature_names()
-            A = X.toarray()
+            # vectorizer = CountVectorizer(min_df=1)
+            # X = vectorizer.fit_transform(self.sentence_list)
+            # # print vectorizer.get_feature_names()
+            # A = X.toarray()
+            self.build_model()
+
             for text in self._text_set:
                 # for test_case ''' test__bag_of_words__compute ''' use the OrderedDict
                 # to check the values with the term_frequency in test_suitcase.resource
                 #
                 # temp_model = collections.OrderedDict(sorted(self.model.items()))
 
-                # temp_model = dict(self.model)
-                # for word in text.wordlist_lower:
-                #     try:
-                #         temp_model[word] += 1
-                #     except KeyError:
-                #         continue
-                # text.features["bag_of_words"] = temp_model.values()
-                end_list = []
-                for i in A[c]:
-                    end_list.append(i)
+                temp_model = dict(self.model)
+                for word in text.wordlist_lower:
+                    try:
+                        temp_model[word] += 1
+                    except KeyError:
+                        continue
+                text.features["bag_of_words"] = temp_model.values()
 
-
-                text.features["bag_of_words"] = end_list
-                c += 1
-            self.bow_model = vectorizer
+            self.bow_model = self.model
+            #     end_list = []
+            #     for i in A[c]:
+            #         end_list.append(i)
+            #
+            #
+            #     text.features["bag_of_words"] = end_list
+            #     c += 1
+            # self.bow_model = vectorizer
 
     def build_model(self):
         for text in self._text_set:
-            self.sentence_list.append(text.text)
-            # for word in text.wordlist_lower:
-            #     if word not in self.stopwords:
-            #         self.model[word] = 0
+            # self.sentence_list.append(text.text)
+            for word in text.wordlist_lower:
+                if word not in self.stopwords:
+                    self.model[word] = 0
         # print self.sentence_list
