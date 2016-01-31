c0d957514b577ad6224539a9fc3363e04ad8bfd5
Jan W <jan@web.de>
2016-01-31 18:39:22 +0100

    added __all__

--- text_classifier/attributes/__init__.py
+++ text_classifier/attributes/__init__.py
@@ -1,1 +1,4 @@
 __author__ = 'jan'
+__all__ = ['adjective','bag_of_words','bag_of_pos','modal',
+           'passive','perfect_tense','readability','sentence_start','tf_idf','veriety_count']
+from bag_of_pos import BagOfPos
