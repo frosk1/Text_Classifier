843b7a5e6ba642ef4f3f0a768b4e8dfe812913f2
Jan W <jan@web.de>
2016-02-01 12:06:58 +0100

    added ressource_path

---
+++ text_classifier/ressource_path.py
@@ -0,0 +1,84 @@
+"""
+Ressource modul for file paths
+
+Modul contains the file paths for korpus data.
+
+Usage :
+
+Change the path to your Korpus folder
+
+    ZA = "/insert path to the Korpus folder/Korpus/ZA/Finales_Korpus/Text_Korpus_aktuell_marked.txt"
+    ....
+"""
+
+# Author Jan Wessling
+
+
+# Korpus files
+ZA = "/home/jan/Development/Korpus/ZA/Finales_Korpus/Text_Korpus_aktuell_marked.txt"
+Fball = "/home/jan/Development/Korpus/Fball/Finales_Korpus/Fussball_korpus_marked.txt"
+
+
+
+
+# Manuelle_Annotation
+testmenge_ZA = "/home/jan/Development/Korpus/ZA/Annotation/testmenge400_ZA.txt"
+trainingsmenge_ZA = "/home/jan/Development/Korpus/ZA/Annotation/trainingsmenge600_ZA.txt"
+testmenge_Fball = "/home/jan/Development/Korpus/Fball/Annotation/testmenge200_Fball.txt"
+trainingsmenge_Fball = "/home/jan/Development/Korpus/Fball/Annotation/trainingsmenge600_Fball.txt"
+
+
+# Train automatisch generierte Annotationen
+
+# ZA Bekleidungs
+sampling_anno_auto_gut1_10k = "/home/jan/Development/Korpus/ZA/Annotation/auto_sample/sampling_anno_auto_gut1_10k.txt"
+sampling_anno_auto_gut2_10k = "/home/jan/Development/Korpus/ZA/Annotation/auto_sample/sampling_anno_auto_gut2_10k.txt"
+sampling_anno_auto_schlecht1_10k = "/home/jan/Development/Korpus/ZA/Annotation/auto_sample/sampling_anno_auto_schlecht1_10k.txt"
+sampling_anno_auto_schlecht2_10k = "/home/jan/Development/Korpus/ZA/Annotation/auto_sample/sampling_anno_auto_schlecht2_10k.txt"
+sampling_anno_man1_10k = "/home/jan/Development/Korpus/ZA/Annotation/auto_sample/sampling_anno_man1_10k.txt"
+sampling_anno_man_all = "/home/jan/Development/Korpus/ZA/Annotation/auto_sample/sampling_anno_man1.txt"
+sampling_anno_man2_10k = "/home/jan/Development/Korpus/ZA/Annotation/auto_sample/sampling_anno_man2_10k.txt"
+sampling_anno_man4_10k = "/home/jan/Development/Korpus/ZA/Annotation/auto_sample/sampling_anno_man4_10k.txt"
+sampling_anno_man_auto_gut_1_10k = "/home/jan/Development/Korpus/ZA/Annotation/auto_sample/sampling_anno_man_auto_gut_1_10k.txt"
+
+# Fball
+fball_sampling_anno_man1_10k = "/home/jan/Development/Korpus/Fball/Annotation/auto_sample/fball_sampling_anno_man1_10k.txt"
+fball_sampling_anno_man_auto_gut1 = "/home/jan/Development/Korpus/Fball/Annotation/auto_sample/fball_sampling_anno_man_auto_gut1.txt"
+fball_sampling_anno_man2 = "/home/jan/Development/Korpus/Fball/Annotation/auto_sample/fball_sampling_anno_man2.txt"
+fball_sampling_anno_auto_gut1_10k = "/home/jan/Development/Korpus/Fball/Annotation/auto_sample/fball_sampling_anno_auto_gut1_10k.txt"
+fball_sampling_anno_auto_gut2 = "/home/jan/Development/Korpus/Fball/Annotation/auto_sample/fball_sampling_anno_auto_gut2.txt"
+fball_sampling_anno_auto_schlecht1 = "/home/jan/Development/Korpus/Fball/Annotation/auto_sample/fball_sampling_anno_auto_schlecht1.txt"
+fball_sampling_anno_auto_schlecht2 = "/home/jan/Development/Korpus/Fball/Annotation/auto_sample/fball_sampling_anno_auto_schlecht2.txt"
+fball_sampling_anno_man4_10k = "/home/jan/Development/Korpus/Fball/Annotation/auto_sample/fball_sampling_man4_10k.txt"
+
+
+# Test with filter
+
+filter_anno_auto_gut_auto_schlecht = "/home/jan/Development/Korpus/ZA/Annotation/filter_anno/filter_anno_auto_gut_auto_schlecht.txt"
+filter_anno_auto_gut_man = "/home/jan/Development/Korpus/ZA/Annotation/filter_anno/filter_anno_auto_gut_man.txt"
+filter_anno_auto_schlecht_man = "/home/jan/Development/Korpus/ZA/Annotation/filter_anno/filter_anno_auto_schlecht_man.txt"
+filter_anno_man_auto_schlecht_auto_gut_auto_schlecht = "/home/jan/Development/Korpus/ZA/Annotation/filter_anno/Rfilter_anno_man_auto_schlecht_auto_gut_auto_schlecht.txt"
+filter_anno_never_same = "/home/jan/Development/Korpus/ZA/Annotation/filter_anno/filter_anno_never_same.txt"
+filter_anno_man_man = "/home/jan/Development/Korpus/ZA/Annotation/filter_anno/filter_anno_man_man.txt"
+filter_anno_JMequal_man_man = "/home/jan/Development/Korpus/ZA/Annotation/filter_anno/filter_anno_JMequal_man_man.txt"
+
+
+# stuff and other annotations
+
+file_test = "/home/jan/Development/Text_Classifier/text_classifier/test_suitcase/test_korpus2.txt"
+file_test2 = "/home/jan/Development/Text_Classifier/text_classifier/test_suitcase/test_annotation2.txt"
+file_test2_2 =" /home/jan/Development/Text_Classifier/text_classifier/test_suitcase/test_annotation2_2.txt"
+file1 = "/home/jan/Development/Text_Classifier/text_classifier/test_suitcase/test_korpus.txt"
+file2 = "/home/jan/Development/Text_Classifier/text_classifier/test_suitcase/test_annotation.txt"
+file3 = "/home/jan/Development/Korpus/ZA/Annotation/annotation_regular_equal_Jan_Mel.txt"
+file4 = "/home/jan/Development/Korpus/ZA/Annotation/annotation_independent_350_Jan.txt"
+file5 = "/home/jan/Development/Korpus/ZA/Annotation/annotation_independent_350_Mel.txt"
+file6 = "/home/jan/Development/Korpus/ZA/Annotation/overall_annotation_80.txt"
+
+file6_test = "/home/jan/Development/Korpus/ZA/Annotation/overall_annotation_80_20pro.txt"
+file7 = "/home/jan/Development/Korpus/ZA/Annotation/overall_annotation_20.txt"
+file8 = "/home/jan/Development/Korpus/ZA/Annotation/overall_annotation_100.txt"
+file6_testing = "/home/jan/Development/Korpus/ZA/Annotation/auto_sample_geteilt/overall_annotation_80.txt"
+file10 = "/home/jan/Development/Korpus/Fball/Annotation/overall_annotation_100.txt"
+file11 = "/home/jan/Development/Korpus/Fball/Annotation/overall_annotation_818.txt"
+
